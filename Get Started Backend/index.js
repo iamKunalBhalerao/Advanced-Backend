9a2c03760bf4a5d9dc94a8df9d39cbcee4c29528
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-04-12 10:08:54 +0530

    Created simple route structure

--- Get Started Backend/index.js
+++ Get Started Backend/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import { signup } from "./src/controllers/signup.controller.js";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server is on PORT:3000");
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/signup", signup);
+
+app.listen(port, () => {
+  console.log(`server is on ${port}`);
 });
---
+++ Get Started Backend/src/controllers/signup.controller.js
@@ -0,0 +1,4 @@
+export const signup = (req, res, next) => {
+  res.send("You are Signed Up");
+  next();
+};
