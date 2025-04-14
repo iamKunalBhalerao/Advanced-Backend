75740b801a05d5f389bd4429d0d27302be24ab5d
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-04-14 10:40:24 +0530

    Utils Code Diccussion

--- Advanced Project Setup/src/index.js
+++ Advanced Project Setup/src/index.js
@@ -1,4 +1,13 @@
 import "dotenv/config";
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running PORT:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection FAILED !!! ", err);
+  });
