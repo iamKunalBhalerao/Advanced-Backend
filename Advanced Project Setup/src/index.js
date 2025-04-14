04c34fe8d8f0647fb48b7b24d9e89de2a495e3a1
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2025-04-14 08:57:29 +0530

    updated index file

--- Advanced Project Setup/src/index.js
+++ Advanced Project Setup/src/index.js
@@ -1,16 +1,4 @@
-import 'dotenv/config'
-import express from "express";
+import "dotenv/config";
 import connectDB from "./db/db.js";
 
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on PORT:${process.env.PORT}`);
-  connectDB();
-});
+connectDB();
